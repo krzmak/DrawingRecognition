009ba9a9801e7093f45d3ff1af76244fc0bb1e0b
krzmak <krzysztof.mak.2001@gmail.com>
2024-09-19 18:22:24 +0200

    Added separation for mulitiple files in folder
    Separates files data from folder into 3 sets: training, validation, test. Separted data is located in coresponding folders

--- PrepareData.py
+++ PrepareData.py
@@ -3,6 +3,22 @@
 import numpy as np
 import math
 import matplotlib.pyplot as plt
+import os
+import json
+
+with open('configuration.json', 'r') as config:
+    location = json.load(config)
+
+training_data_path = location.get("trainig data path")
+validation_data_path = location.get("validation data path")
+test_data_path = location.get("test data path")
+raw_data_path = location.get("raw data path")
+
+
+print(f"train data path: {training_data_path}")
+print(f"val data path: {validation_data_path}")
+print(f"test data path: {test_data_path}")
+print(f"raw data path: {raw_data_path}")
 
 def LoadNumpyFile(file):
     data = np.load(file)
@@ -26,28 +42,33 @@ def SelectData(drawings):
 
     return training_data, validation_data, test_data
 
-def SaveData(drawings, train, file_name):
-    output = drawings[train]
-    np.save(file_name, output)
+def SaveData(drawings, train, val, test, train_path, val_path, test_path, file_name):
+    train_output = drawings[train]
+    val_output = drawings[val]
+    test_output = drawings[test]
+    file_name_without_ext = os.path.splitext(file_name)[0]
+    np.save(os.path.join(train_path, file_name_without_ext + '_train.npy'), train_output)
+    np.save(os.path.join(val_path, file_name_without_ext + '_val.npy'), val_output)
+    np.save(os.path.join(test_path, file_name_without_ext + '_test.npy'), test_output)
 
+def SeparateFiles(input_directory):
+    directory = os.fsencode(input_directory)
+    
+    for file in os.listdir(directory):
+        file_name = os.fsdecode(file)
+        file_path = input_directory + '/' + file_name
+        drawings = LoadNumpyFile(file_path)
+        numberofdrawings = NumberOfDrawings(drawings)
+        data = SelectData(drawings)
+        train = data[0]
+        val = data[1]
+        test = data[2]
+        SaveData(drawings, train, val, test, training_data_path, validation_data_path, test_data_path, file_name)
 
-file = 'cnn_data/data_raw/full_numpy_bitmap_cat.npy'
-file_save = 'test.npy'
+        print("file name:", file_name)
+        print("number of images in train data:", np.size(train))
+        print("number of images in val data:", np.size(val)) 
+        print("number of images in test data:", np.size(test)) 
+        print("number of images in this file:", numberofdrawings) 
 
-drawings = LoadNumpyFile(file)
-numberofdrawings = NumberOfDrawings(drawings)
-
-data = SelectData(drawings)
-
-train = data[0]
-val = data[1]
-test = data[2]
-
-SaveData(drawings, train, file_save)
-
-print(train)
-
-print("number of images in train data:", np.size(train))
-# print("number of images in train data:", numberofdrawings(val)) 
-# print("number of images in train data:", numberofdrawings(test)) 
-print("number of images in this file:", numberofdrawings) 
+SeparateFiles(raw_data_path)
