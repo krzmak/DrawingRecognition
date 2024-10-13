e2ee1fd7b051a9cd3f299dff90463a528f95db45
krzmak <krzysztof.mak.2001@gmail.com>
2024-10-13 20:37:36 +0200

    first testings of cnn model

--- CnnTesting.py
+++ CnnTesting.py
@@ -81,26 +81,128 @@ training_dataset = NumpyDrawingsDataset(training_data_path, transform= data_tran
 validation_dataset = NumpyDrawingsDataset(validation_data_path, transform= data_transforms)
 testing_dataset = NumpyDrawingsDataset(test_data_path, transform= data_transforms)
 
-print(training_dataset.data[2])
-print(np.shape(training_dataset.data[1]))
+train_loader = torch.utils.data.DataLoader(training_dataset, batch_size=64, shuffle=True)
+val_loader = torch.utils.data.DataLoader(validation_dataset, batch_size=64, shuffle=True)
 
-
-"""class CnnModel(nn.Module):
+class CnnModel(nn.Module):
 
     def __init__(self, number_of_classes = 4):
         super().__init__()
         self.conv1 = nn.Conv2d(1, 64, kernel_size=5, stride=1, padding=2)
-        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
-        self.conv2 = nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1)
-        self.conv3 = nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1)
+        self.conv2 = nn.Conv2d(64, 128, kernel_size=5, stride=1, padding=2)
+        self.conv3 = nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1)
+        self.conv4 = nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1)
+        self.conv5 = nn.Conv2d(256, 128, kernel_size=3, stride=1, padding=1)
         self.pool = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
-        self.fc1 = nn.Linear(128 * 16 * 16, 512)
-        self.fc2 = nn.Linear(512, num_classes)
-
+        self.fc1 = nn.Linear(128 * 8 * 8, 1024)
+        self.fc2 = nn.Linear(1024, 512)
+        self.fc3 = nn.Linear(512, number_of_classes)
 
     def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = F.relu(self.conv3(x))
+        x = F.relu(self.conv4(x))
+        x = self.pool(F.relu(self.conv5(x)))
+        x = x.view(-1, 128 * 8 * 8)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x 
 
-    """
+cnn_model = CnnModel(number_of_classes=4)
+cnn_model = cnn_model.to(device)
+loss_fn = nn.CrossEntropyLoss()
+optimizer = optim.SGD(cnn_model.parameters(), lr=0.001, momentum=0.9, weight_decay=0.003)
+
+def train_nn(model, train_loader, val_loader, criterion, optimizer, n_epochs=20):
+    best_acc = 0
+
+    for epoch in range(n_epochs):
+        print("Epoch number %d " % (epoch + 1))
+        model.train()
+        running_loss = 0.0
+        running_correct = 0.0
+        total = 0
+        for data in train_loader:
+            images, labels = data
+            images = images.to(device)
+            labels = labels.type(torch.LongTensor)
+            labels = labels.to(device)
+            total += labels.size(0)
+
+            optimizer.zero_grad()
+
+            outputs = model(images)
+
+            _, predicted = torch.max(outputs.data, 1)
+
+            loss = criterion(outputs, labels)
+            
+            loss.backward()
+
+            optimizer.step()
+
+            running_loss += loss.item()
+            running_correct += (labels == predicted).sum().item()
+
+        epoch_loss = running_loss / len(train_loader)
+        epoch_acc = 100 * running_correct / total
+
+        print("         -Training dataset. Got %d out of %d images correctly (%.3f%%). Epoch loss: %.3f" % (running_correct, total, epoch_acc, epoch_loss))
+
+        test_dataset_acc = evaluate_model_on_test_set(model, val_loader)
+
+        if test_dataset_acc > best_acc:
+            best_acc = test_dataset_acc
+            save_checkpoint(model, epoch, optimizer, best_acc)
+
+    print("Finished")
+    return model
+
+def evaluate_model_on_test_set(model, val_loader):
+    model.eval()
+    predicted_correctly_on_epoch = 0
+    total = 0
+
+    with torch.no_grad():
+        for data in val_loader:
+            images, labels = data
+            labels = labels.type(torch.LongTensor)
+            images = images.to(device)
+            labels = labels.to(device)
+            total += labels.size(0)
+
+            outputs = model(images)
+            
+            _, predicted = torch.max(outputs.data, 1)
+
+            predicted_correctly_on_epoch += (predicted == labels).sum().item()
+
+    epoch_acc = 100.0 * predicted_correctly_on_epoch / total
+
+    print("         -Validation dataset. Got %d of %d images correctly ((%.3f%%)" % (predicted_correctly_on_epoch, total, epoch_acc))
+
+    return epoch_acc
+
+def save_checkpoint(model, epoch, optimizer, best_acc):
+    state = {
+        'epoch': epoch + 1,
+        'model': model.state_dict(),
+        'best_accuracy': best_acc,
+        'optimizer': optimizer.state_dict(),
+    }
+    torch.save(state, 'best_model_cnn_checkpoint.pth.tar')
+
+train_nn(model=cnn_model, train_loader=train_loader, val_loader=val_loader, criterion=loss_fn, optimizer=optimizer, n_epochs=40)
+
+checkpoint = torch.load('best_model_cnn_checkpoint.pth.tar')
+
+cnn_model.load_state_dict(checkpoint['model'])
+cnn_model = cnn_model.to(device)
+
+torch.save(cnn_model, 'cnn_model.pth')
+
 
 def show_images_of_given_label(dataset, label):
     batch_size = 64
@@ -117,4 +219,3 @@ def show_images_of_given_label(dataset, label):
     plt.imshow(np.transpose(grid, (1,2,0)))
     plt.show()
     
-show_images_of_given_label(training_dataset, 1)
